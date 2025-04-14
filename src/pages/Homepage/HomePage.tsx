dc86b31a28b7da3d0b1c116d77eabe8b5dbf7354
Dante0214 <rn_chic@naver.com>
2025-04-15 01:03:35 +0900

    feat: 파일명 변경

--- src/pages/Homepage/HomePage.tsx
+++ src/pages/Homepage/HomePage.tsx
@@ -1,7 +1,7 @@
 import Banner from "./components/Banner/Banner";
 import PopulareMovieSlide from "./components/MovieSlides/PopulareMovieSlide";
 import TopRatedMovieSlide from "./components/MovieSlides/TopRatedMovieSlide";
-import UpcomingMovieSlide from "./components/MovieSlides/UpcomingRatedMovieSlide copy";
+import UpcomingMovieSlide from "./components/MovieSlides/UpcomingRatedMovieSlide";
 
 const HomePage = () => {
   return (
